8467b15f2ecdf64a77c75b429d1f98878de1a822
Petyo Kunchev <ptkunchev@gmail.com>
2020-10-13 18:51:33 +0300

    added metadata to the module

--- setup_go.py
+++ setup_go.py
@@ -1,6 +1,8 @@
 #! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+
 """Download and install Go on Linux, list all available versions on the
-   Go website
+   Go website, select version to install and pass it as an argument
 
 Attributes:
     chunk_size (int): Chunks size of the package, required for tqdm
@@ -8,10 +10,20 @@ Attributes:
     go_local (str): Local download folder on the filesystem
     go_url (str): URL with desired go package
 """
+
+
+__author__ = 'Petyo Kunchev'
+__version__ = '1.0.1'
+__maintainer__ = 'Petyo Kunchev'
+__email__ = 'ptkunchev@gmail.com'
+__status__ = 'Development'
+__license__ = 'MIT'
+
+
+import os
 from typing import List, Any
 
 try:
-    import os
     import argparse
     import requests
     import httplib2
@@ -28,12 +40,14 @@ go_url: str = 'https://golang.org/dl/go1.15.2.linux-amd64.tar.gz'
 go_local: str = '/tmp/'
 chunk_size: int = 1024
 
+
 # TODO: Implement color print based on message type - green for ok,
 #  red for error messages and blue for informational messages
 
 # TODO: Validate format of the input parameter for the Go version - must
 #  follow x.y, x.yy, x.y.z or x.yy.z pattern, where x y and z are digits
 #  0 to 9
+
 
 def check_exists_dl_folder(folderpath):
     """Check if the local download folder exists
@@ -140,7 +154,8 @@ def get_go(url, location):
 
 
 def main():
-    """Main function, entry point of program
+    """Main function, entry point of program, argparser is used here in
+    combination with the functions defined in this module
     """
     download_url = None
     desired_version = None
